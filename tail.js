bc8cacde7d94a755b3d5ca70e869a3841b0e99cd
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-04 20:18:48 +0000

    spacing error

--- tail.js
+++ tail.js
@@ -1,8 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (!(actual.length === expected.length)) {
+  if (actual.length === expected.length) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  } for (let i = 0; i < actual.length; i++) {
+  } 
+  for (let i = 0; i < actual.length; i++) {
     if (actual[i] === expected[i]) {
       console.log(`Assertion Passed: ${actual} === ${expected}`);
     } else {
@@ -14,7 +15,7 @@ const assertEqual = function(actual, expected) {
 const tail = function(array) {
   let newArray = array;
   newArray.shift();
-  return (newArray);
+  return newArray;
 };
 // TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
