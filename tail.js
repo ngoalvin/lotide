f346b8d2bdf3a2d62eab62022ea10c1e629511b3
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-04 20:06:53 +0000

    implementing tail.js

---
+++ tail.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (!(actual.length === expected.length)) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } for (let i = 0; i < actual.length; i++) {
+    if (actual[i] === expected[i]) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+
+const tail = function(array) {
+  let newArray = array;
+  newArray.shift();
+  return (newArray);
+};
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, (["Lighthouse", "Labs"])); // => will always fail!
