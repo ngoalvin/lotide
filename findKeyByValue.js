9921d09059c7d6028379556fab7847e6bcb0c1da
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-06 18:44:38 +0000

    implemented findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const findKeyByValue = function(obj, show) {
+  const objKeys = Object.keys(obj);
+  for (let key of objKeys) {
+    if (obj[key] === show) {
+      return key;
+    }
+  }
+  // return undefined;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
