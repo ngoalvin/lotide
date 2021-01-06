7579a953cfa86b9e7b65c73a46a278343783343a
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-06 17:12:00 +0000

    implemented countOnly.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,17 +8,16 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let condition = true;
   if(array1.length !== array2.length || !(Array.isArray(array1)) || !(Array.isArray(array2))) {
-    condition = false;
+    return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        condition = false;
+        return false;
       }
     }
   }
-  return condition;
+  return true;
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
