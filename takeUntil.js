102d52ec91c9b920445f903cd7df4eef38d4708e
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-07 19:22:43 +0000

    implement takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -20,8 +20,27 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2]);
-assertArraysEqual([1, 2, 3], []);
+const takeUntil = function(array, callback) {
+  let output = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return output;
+    } else {
+      output.push(item);
+    }
+  }
+  return output;
+};
 
 
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [1,2,5,7,2]);
